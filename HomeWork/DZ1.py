cdb150a825a8f10a4736f0b00dc64253b23d21f9
zSunset <85899844+zSunset@users.noreply.github.com>
2022-05-17 23:52:47 +0300

    Revert "DZ1"

--- HomeWork/DZ1.py
+++
@@ -1,9 +0,0 @@
-def duration(sec):
-    sec = sec % (24 * 3600)
-    hour = sec // 3600
-    sec % 3600
-    min = sec // 60
-    sec %= 60
-    return "%02d:%02d:%02d" % (hour, min, sec)
-s = 5800
-print(duration(s))
